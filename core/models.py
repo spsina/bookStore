992e354a8f38a976d1fdf845e0f18f1aab2c495b
Ali Parvizi <snparvizi75@gmailc.om>
2021-02-23 17:05:59 +0330

    book model fields

--- core/models.py
+++ core/models.py
@@ -19,3 +19,21 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=120, blank=True, null=True)
     address = models.CharField(max_length=1024, blank=True, null=True)
     postal_code = models.CharField(max_length=10, blank=True, null=True)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120, blank=True, null=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=1024)
+    description = models.TextField(blank=True, null=True)
+
+    authors = models.ManyToManyField(Person, related_name='authored_books')
+    editors = models.ManyToManyField(Person, blank=True, related_name="edited_books")
+    translators = models.ManyToManyField(Person, blank=True, related_name="translated_books")
+
+    isbn = models.CharField(max_length=20, blank=True, null=True)
+
+    image = models.ImageField(blank=True, null=True)
