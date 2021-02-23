98df2be81a9a750e36f6c8ab19e15e5b6805293a
Ali Parvizi <snparvizi75@gmailc.om>
2021-02-23 16:46:50 +0330

    secret.py

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r$tlj25=rq8)*fb!q500pp)jxj!7-k&z_8dj49@*bon2wds9rx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,15 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BookStore.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
