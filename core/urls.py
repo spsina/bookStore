1258fbab538ff873bd31cb91507fd057a49e5600
Ali Parvizi <snparvizi75@gmail.com>
2021-02-25 02:01:50 +0330

    user profile get or create

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserProfileCreateView, UserProfileRUDView
+from .views import UserProfileCreateView, UserProfileRUDView, GetOrCreateUserProfile
 
 urlpatterns = [
     path('user_profile/create/', UserProfileCreateView.as_view()),
     path('user_profile/<int:user_profile_id>/', UserProfileRUDView.as_view()),
+    path('user_profile/<phone_number>/get_or_create/', GetOrCreateUserProfile.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -16,3 +16,22 @@ class UserProfileRUDView(generics.RetrieveUpdateAPIView):
 
     lookup_field = 'pk'
     lookup_url_kwarg = 'user_profile_id'
+
+
+class GetOrCreateUserProfile(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    
+    def get_object(self):
+        try:
+            user_profile = UserProfile.objects.get(phone_number=self.kwargs.get('phone_number'))
+            return user_profile
+        except UserProfile.DoesNotExist:
+            serializer = UserProfileSerializer(data={
+                'phone_number': self.kwargs.get('phone_number'),
+            }, context={
+                'request': self.request,
+                'view': self
+            })
+            serializer.is_valid(raise_exception=True)
+            return serializer.save()
+    
