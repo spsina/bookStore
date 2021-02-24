5161dffbf18b4123fe8a0dbd5616616c32523ab7
Ali Parvizi <snparvizi75@gmail.com>
2021-02-25 02:48:49 +0330

    search fields

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, Book, Basket, Person
-
+from .models import UserProfile, Book, Basket, Person, Invoice
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['pk', 'phone_number', 'first_name', 'last_name']
@@ -9,15 +8,15 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'description', 'price', 'discount', 'count', 'sold', 'final_price']
-
+    search_fields = ['pk', 'title', 'description', 'price', ]
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['pk', 'first_name', 'last_name']
-
+    search_fields = ['first_name', 'last_name']
 
 class BasketAdmin(admin.ModelAdmin):
     list_display = ['pk', 'user_profile', 'book', 'count']
-
+    filter_fields = ['user_profile', 'book',]
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Book, BookAdmin)
