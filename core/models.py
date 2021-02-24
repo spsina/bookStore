24fbc917373ef6f5674257debb0f30df1856c280
Ali Parvizi <snparvizi75@gmial.com>
2021-02-24 23:41:26 +0330

    basket

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 class UserProfile(models.Model):
@@ -20,20 +21,52 @@ class UserProfile(models.Model):
     address = models.CharField(max_length=1024, blank=True, null=True)
     postal_code = models.CharField(max_length=10, blank=True, null=True)
 
+    def __str__(self):
+        return "%d - %s - %s %s" % (self.pk, self.phone_number, self.first_name, self.last_name)
+
 
 class Person(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120, blank=True, null=True)
 
+    def __str__(self):
+        return "%d - %s %s" % (self.pk, self.first_name, self.last_name)
+
 
 class Book(models.Model):
+    # display info
     title = models.CharField(max_length=1024)
     description = models.TextField(blank=True, null=True)
 
+    # people
     authors = models.ManyToManyField(Person, related_name='authored_books')
     editors = models.ManyToManyField(Person, blank=True, related_name="edited_books")
     translators = models.ManyToManyField(Person, blank=True, related_name="translated_books")
 
+    # id
     isbn = models.CharField(max_length=20, blank=True, null=True)
 
+    # header image
     image = models.ImageField(blank=True, null=True)
+
+    # number of this books available
+    count = models.PositiveIntegerField(default=0)
+
+    # instead of deleting book objects, set this flag to True
+    is_delete = models.BooleanField(default=False)
+
+    @property
+    def sold(self):
+        return 0
+
+    def __str__(self):
+        return "%d - %s" % (self.pk, self.title)
+
+
+class Basket(models.Model):
+    user_profile = models.ForeignKey(UserProfile, related_name="orders", on_delete=models.PROTECT)
+    book = models.ForeignKey(Book, related_name="orders", on_delete=models.PROTECT)
+    count = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return "%d - %s | %s %d" % (self.pk, self.user_profile, self.book, self.count)
