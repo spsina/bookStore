3603ff175349e5a323399f2ac23c8f4539dd50ec
Ali Parvizi <snparvizi75@gmial.com>
2021-02-24 23:55:57 +0330

    secert sample

---
+++ BookStore/secret-sample.py
@@ -0,0 +1,11 @@
+SECRET_KEY = ""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
