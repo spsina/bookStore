66abaab485c03fc0944a58d6758c073943d61946
Ali Parvizi <snparvizi75@gmial.com>
2021-02-25 16:02:25 +0330

    media root bug fix

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -115,4 +115,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
