b518603ee66b62f59a70d3ef3fb805a2870cdfaa
Ali Parvizi <snparvizi75@gmailc.om>
2021-02-26 19:32:33 +0330

    bug fix

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
-    'rest_framework'
+    'rest_framework',
     'corsheaders',
 ]
 
