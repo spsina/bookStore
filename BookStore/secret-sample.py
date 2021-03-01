b1307853c39844f5a437acc27829d155e47bccd9
Ali Parvizi <snparvizi75@gmailc.om>
2021-03-01 16:26:33 +0330

    kavenegar send sms

--- BookStore/secret-sample.py
+++ BookStore/secret-sample.py
@@ -1,11 +1,28 @@
-SECRET_KEY = ""
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '',
         'USER': '',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '',
         'PORT': '',
     }
 }
+
+VANDAR_API_KEY = ""
+KAVENEGAR_API_KEY = ""
