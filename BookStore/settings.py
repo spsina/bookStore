b336ae8e5abf43a4ac28da7c386e140f50c3f8a7
Ali Parvizi <snparvizi75@gmailc.om>
2021-03-02 17:53:06 +0330

    move debug flag to secret

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .secret import SECRET_KEY, DATABASES, VANDAR_API_KEY, KAVENEGAR_API_KEY
+from .secret import SECRET_KEY, DATABASES, VANDAR_API_KEY, KAVENEGAR_API_KEY, DEBUG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
                  'abee.ir',
